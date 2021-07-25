8a1b6038dea840a641901208b05a63e03a98bfd4
Shrey <shrey9april@gmail.com>
2021-07-25 19:25:26 +0530

    Refactor: header changes and prettier configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
-import Home from './Home'
-import EditNote from './EditNote'
-import NotFound from './NotFound'
+import Home from "./Home"
+import Header from "./Header"
+import EditNote from "./EditNote"
+import NotFound from "./NotFound"
 
 const App = () => {
    return (
       <>
-         <header className="main-head">
-            <div className="container">
-               <h1>Notes App</h1>
-               <span>Take notes and never forget</span>
-            </div>
-         </header>
+         <Header />
          <main>
             <BrowserRouter>
                <Switch>
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+   return (
+      <header className="main-head">
+         <div className="container">
+            <h1>Notes App</h1>
+            <span>Take notes and never forget</span>
+         </div>
+      </header>
+   )
+}
+
+export default Header
