b723a32a7cac9dda0d31d8342c4c46ca41d94f8d
Shrey <shrey9april@gmail.com>
2021-07-28 16:03:49 +0530

    Feat: Navigate to home on logo click

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Header = () => {
    return (
       <header className="main-head">
          <div className="container">
-            <h1>Notes App</h1>
-            <span>Take notes and never forget</span>
+            <Link>
+               <h1>Notes App</h1>
+               <span>Take notes and never forget</span>
+            </Link>
          </div>
       </header>
    )
